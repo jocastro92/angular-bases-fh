7363258fe0ca5e5f8fcab9575c8d9ee6984c2633
Jordan Castro <jjccacca9192@gmail.com>
2023-05-20 23:04:27 -0600

    actualizacion de curso

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MainPageComponent } from "./pages/main-page.component";
+import { ListComponent } from "../dbz/components/list/list.component";
+import { AddCharacterComponent } from "./components/add-character/add-character.component";
 
 
 @NgModule({
-  declarations:[
-    MainPageComponent
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
-  imports:[
-      CommonModule
+  imports: [
+    CommonModule
   ],
-  exports:[
+  exports: [
     MainPageComponent
   ]
 })
-export class DbzModule{}
+export class DbzModule { }
